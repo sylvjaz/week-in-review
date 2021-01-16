c2a66d9ba4e164b5ce47cd75a705581491f4877d
Sylvja <conneradamson@comcast.net>
2021-01-15 18:16:17 -0800

    Created a result for program language

--- Jqueryproject/js/script.js
+++ Jqueryproject/js/script.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
     const knowledge = $("select#knowlede").val();
     const movies = $("select#movies").val();
     event.preventDefault();
+    let programlanguage; 
     if (age == 16 && hours >= 7) {
-    let programlanguage = "Javascript";  
+    programlanguage = "Javascript";  
     }
-  $("#language").text("Javascript");
+    $("#language").text(programlanguage);
   });
+  console.log("1st number:");
 });
 //Javascript: We want to have an age of 16, 6 hours on the computer or more, hobbies include being inside or on computer, favorite movie is the Matrix.
